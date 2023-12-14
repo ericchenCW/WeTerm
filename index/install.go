3934e763cd1a77c68e48db6b21babd34c88df3a4
ericchen <ericchen@canway.net>
2023-12-14 16:08:46 +0800

    up

--- index/install.go
+++ index/install.go
@@ -1,21 +1,37 @@
 package index
 
-import "weterm/model"
+import (
+	"weterm/model"
+	"weterm/pages/example"
+)
 
 var installMenu = []MenuItem{
 	{
 		Name: "单机版",
 		Action: func(bs *model.AppModel) {
+			example.SetUpFormSamplePage(bs)
+			bs.CorePages.SwitchToPage("form_sample")
 		},
 	},
 	{
 		Name: "标准版(3节点)",
 		Action: func(bs *model.AppModel) {
+			example.SetUpFormSamplePage(bs)
+			bs.CorePages.SwitchToPage("form_sample")
 		},
 	},
 	{
 		Name: "高可用版(7节点)",
 		Action: func(bs *model.AppModel) {
+			example.SetUpFormSamplePage(bs)
+			bs.CorePages.SwitchToPage("form_sample")
+		},
+	},
+	{
+		Name: "自定义安装",
+		Action: func(bs *model.AppModel) {
+			example.SetUpFormSamplePage(bs)
+			bs.CorePages.SwitchToPage("form_sample")
 		},
 	},
 }
