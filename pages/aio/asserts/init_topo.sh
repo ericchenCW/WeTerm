815624bc90cc4ebef42d7f91a5b2ceeeedcd1ec8
eric <deadgay0625@gmail.com>
2024-10-24 19:52:41 +0800

    feat: 重新初始化拓扑
    重新初始化拓扑，清理存量拓扑记录，并重启cmdb服务。

--- pages/aio/asserts/init_topo.sh
+++ pages/aio/asserts/init_topo.sh
@@ -20,13 +20,13 @@ info_echo() {
     echo -e "[blue]$1[white]"
 }
 step_echo "init topo"
-# 清理存量拓扑记录
 mongo -u $BK_CMDB_MONGODB_USERNAME -p $BK_CMDB_MONGODB_PASSWORD mongodb://$LAN_IP:$BK_CMDB_MONGODB_PORT/cmdb --authenticationDatabase cmdb << "EOF"
 db.cc_ServiceTemplate.remove({"bk_biz_id":2})
 db.cc_ProcessTemplate.remove({"bk_biz_id":2})
 db.cc_SetTemplate.remove({"bk_biz_id":2})
 db.cc_SetBase.remove({$and:[{"bk_set_id":{$gt:2}},{"bk_biz_id":{$eq:2}}]},{"bk_set_id":1,"bk_set_name":1,"bk_biz_id":1,"bk_biz_name":1});
 EOF
+step_echo "restart cmdb"
 /data/install/bkcli restart cmdb
 i=1
 until /data/install/bkcli check cmdb 2>&1 >/dev/null;do
@@ -34,5 +34,8 @@ until /data/install/bkcli check cmdb 2>&1 >/dev/null;do
     i=$i+1
     sleep 10
 done
+sleep 10
+step_echo "start init topo"
 # 重新初始化拓扑
 /data/install/bkcli initdata topo
+step_echo "初始化成功"
